417db54baf7b8256c1e573b841998ad13c4479f4
fahd-ops2 <okyby2018@gmail.com>
2025-04-05 15:06:43 +0000

    feat : add circuit breaker for managing fault tolerance

--- account-service/src/main/java/io/fahd/accountservice/clients/CustomerRestClient.java
+++ account-service/src/main/java/io/fahd/accountservice/clients/CustomerRestClient.java
@@ -2,6 +2,7 @@ package io.fahd.accountservice.clients;
 
 
 import io.fahd.accountservice.model.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,5 +11,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface CustomerRestClient {
 
     @GetMapping("/api/v1/customers/{id}")
+    @CircuitBreaker(name = "customer-service", fallbackMethod = "getDefaultCustomer")
     Customer findCustomerById(@PathVariable Long id);
+
+    default Customer getDefaultCustomer(Long id, Exception exception){
+        return Customer.builder()
+                .id(id)
+                .firstName("not available")
+                .lastName("not available")
+                .email("not available")
+                .build();
+    }
 }
